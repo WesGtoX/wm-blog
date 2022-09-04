3ce088fe95dd092186a4f7b1831720a093dcf870
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-09-03 23:51:49 -0300

    feat: adding about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,11 @@ import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const AboutPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Home</h1>
+    <Seo title="About" />
+    <h1>About Page</h1>
   </Layout>
 )
 
-export default IndexPage
+export default AboutPage
