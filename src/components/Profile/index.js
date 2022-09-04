8537684e53acc431f87d0bebf3af21b8711a6ce8
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-09-03 23:52:02 -0300

    feat: adding profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Wesley Mendes</h1>
+    <h2>Senior Backend Engineer</h2>
+    <p>
+      Membro do Core Team da Empresa Atados, escrevo sobre o mundo Backend e
+      outras coisas mais.
+    </p>
+  </div>
+)
+
+export default Profile
