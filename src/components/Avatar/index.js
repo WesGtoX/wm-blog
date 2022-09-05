d411d2f5d61592091fad16fa8f71860285191ffe
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-09-05 00:19:35 -0300

    feat: change avatar image to use with StaticImage

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,21 +1,14 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Avatar = () => {
-  const { avatarImage } = useStaticQuery(
-    graphql`
-      query {
-        avatarImage: file(relativePath: { eq: "profile-photo.jpg" }) {
-          childImageSharp {
-            gatsbyImageData(width: 60, layout: CONSTRAINED, placeholder: BLURRED)
-          }
-        }
-      }
-    `,
+  return (
+    <StaticImage
+      src="../../images/profile-photo.jpg"
+      alt="Profile Photo"
+      placeholder="blurred"
+    />
   )
-
-  return <GatsbyImage image={avatarImage.childImageSharp.gatsbyImageData} />
 }
 
 export default Avatar
