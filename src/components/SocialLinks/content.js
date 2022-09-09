453858b56b35de1a4d0ae622602a42fe7d77e499
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-09-09 02:55:08 -0300

    feat: adding social links icons and content

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,26 @@
+const handle = "wesgtox"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Linkedin",
+    url: `https://www.linkedin.com/in/${handle}/`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${handle}`,
+  },
+  {
+    label: "YouTube",
+    url: "https://www.youtube.com/channel/UCXwcNJg6iDj3s7MLSJJ9EXQ",
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/${handle}/`,
+  },
+]
+
+export default links
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,17 @@
+import {
+  Github,
+  Linkedin,
+  Twitter,
+  YouTube,
+  Instagram,
+} from "styled-icons/boxicons-logos"
+
+const Icons = {
+  Github,
+  Linkedin,
+  Twitter,
+  YouTube,
+  Instagram,
+}
+
+export default Icons
