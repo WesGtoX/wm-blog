f803b120444b85dc27a21d9635d69eba1f0cc106
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-09-10 01:18:06 -0300

    feat: adding fixed menu bar on the right

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import Sidebar from "../Sidebar"
+import MenuBar from "../MenuBar"
 
 import * as S from "./styled"
 
@@ -12,6 +13,7 @@ const Layout = ({ children }) => {
       <GlobalStyles />
       <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
+      <MenuBar />
     </S.LayoutWrapper>
   )
 }
