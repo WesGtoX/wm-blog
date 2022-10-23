8adea80803025c1cf03fc0998e61ac2da54086c1
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-10-23 17:39:52 -0300

    fix: remove empty folder from file system settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `posts`,
         path: `${__dirname}/posts`,
       },
